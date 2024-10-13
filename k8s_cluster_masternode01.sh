383cd19402d35ce4a758dfe5ea238975b8152407
MehulSolanki <er.mehulsolanki1887@gmail.com>
2024-10-13 20:59:58 +0530

    Update k8s_cluster_masternode01.sh

--- k8s_cluster_masternode01.sh
+++ k8s_cluster_masternode01.sh
@@ -5,7 +5,7 @@ sudo hostnamectl set-hostname masternode01
 sudo swapoff -a 
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-sudo tee /etc/modules-load.d/containerd.conf <<EOF
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
@@ -13,47 +13,43 @@ EOF
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
-sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
+net.ipv4.ip_forward                 = 1
 EOF
 
 sudo sysctl --system
-
-sudo apt-get install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+apt-get install ca-certificates curl gnupg lsb-release -y
 
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \ "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
-
-sudo apt-get install -y containerd.io
-
+sudo apt-get update -y
+sudo apt-get install containerd.io -y
 
 sudo containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
-
 sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
 
-
 sudo systemctl restart containerd
-
 sudo systemctl enable containerd
 
-
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
 
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
 
 sudo apt-get update
-
 sudo apt-get install -y kubelet kubeadm kubectl
-
 sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo systemctl daemon-reload
+sudo systemctl start kubelet
+sudo systemctl enable kubelet.service
 
 sudo kubeadm init --apiserver-advertise-address=192.168.1.11 --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors=all   
 
