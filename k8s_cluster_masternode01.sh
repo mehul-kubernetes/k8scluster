9a2a6be3157f2bef3347bd2cfdf0620548a03413
MehulSolanki <er.mehulsolanki1887@gmail.com>
2024-10-13 23:23:36 +0530

    Update k8s_cluster_masternode01.sh

--- k8s_cluster_masternode01.sh
+++ k8s_cluster_masternode01.sh
@@ -25,7 +25,9 @@ sudo apt install -y curl gnupg2 software-properties-common apt-transport-https c
 sudo mkdir -p /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-sudo echo \ "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update -y
 sudo apt-get install containerd.io -y
