cdd9f22a8b443ddaf920f34df48a9d0024b8ef02
MehulSolanki <er.mehulsolanki1887@gmail.com>
2024-10-13 23:29:07 +0530

    Update k8s_cluster_workernode02.sh

--- k8s_cluster_workernode02.sh
+++ k8s_cluster_workernode02.sh
@@ -1,11 +1,11 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install net-tools 
+sudo apt-get  update
+sudo apt-get upgrade
 sudo hostnamectl set-hostname workernode02
+
 sudo swapoff -a 
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-sudo tee /etc/modules-load.d/containerd.conf <<EOF
+cat <<EOF | sudo tee /etc/modules-load.d/kubernetes.conf
 overlay
 br_netfilter
 EOF
@@ -13,44 +13,42 @@ EOF
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
-sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
+cat <<EOF | sudo tee /etc/sysctl.d/kubernetes.conf
+net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
+net.ipv4.ip_forward                 = 1
 EOF
 
 sudo sysctl --system
-
 sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
 
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# echo \ "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt update
-
-sudo apt install -y containerd.io
-
+sudo apt-get update -y
+sudo apt-get install containerd.io -y
 
 sudo containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
-
 sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
 
-
 sudo systemctl restart containerd
-
 sudo systemctl enable containerd
 
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
 
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
-
-sudo apt install -y kubelet kubeadm kubectl
-
+sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo systemctl daemon-reload
+sudo systemctl start kubelet
+sudo systemctl enable kubelet.service
   
