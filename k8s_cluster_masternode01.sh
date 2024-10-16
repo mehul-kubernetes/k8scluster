65088190f19df314e78ad3ac2b1e28a4936ae73d
MehulSolanki <er.mehulsolanki1887@gmail.com>
2024-10-16 12:36:15 +0530

    Update k8s_cluster_masternode01.sh

--- k8s_cluster_masternode01.sh
+++ k8s_cluster_masternode01.sh
@@ -59,9 +59,10 @@ sudo mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
 
 kubeadm token create --print-join-command
 
+# kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 # sudo kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
 # kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
