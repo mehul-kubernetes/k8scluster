4910d8ef19bdea91e20c0efdb778d598fec0859b
MehulSolanki <er.mehulsolanki1887@gmail.com>
2024-10-17 15:01:37 +0530

    Update k8s_cluster_masternode01.sh

--- k8s_cluster_masternode01.sh
+++ k8s_cluster_masternode01.sh
@@ -59,7 +59,7 @@ sudo mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-kubectl apply -f https://raw.githubusercontent.com/weaveworks/weave/refs/heads/master/prog/weave-kube/weave-daemonset-k8s-1.8.yaml
+sudo kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
 
 kubeadm token create --print-join-command
 
