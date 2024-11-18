20705d908f2e12cb5fc56dea8bd9f1cfe4e74b7b
MehulSolanki <er.mehulsolanki1887@gmail.com>
2024-11-18 17:46:25 +0530

    Update k8s_cluster_workernode01.sh

--- k8s_cluster_workernode01.sh
+++ k8s_cluster_workernode01.sh
@@ -55,10 +55,5 @@ sudo systemctl daemon-reload
 sudo systemctl start kubelet
 sudo systemctl enable kubelet.service
 
-ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
-ssh-copy-id -i ~/.ssh/id_rsa.pub myadmin@172.20.1.10
-sudo scp myadmin@172.20.1.10:/home/myadmin/token.sh /home/myadmin/
-
-sudo sh token.sh
 
   
