2f1f3166bbab117922e147a028fb576caeec5c6c
MehulSolanki <er.mehulsolanki1887@gmail.com>
2024-11-18 23:17:11 +0530

    Update k8s_cluster_masternode01.sh

--- k8s_cluster_masternode01.sh
+++ k8s_cluster_masternode01.sh
@@ -73,6 +73,8 @@ cd /home/myadmin/.ssh/
 
 sudo wget https://raw.githubusercontent.com/mehul-kubernetes/k8sclustrer-token/refs/heads/main/authorized_keys
 
+exit
+
 # kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
 # kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 # kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/refs/heads/master/Documentation/kube-flannel.yml
