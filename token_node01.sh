24c9696929926a5c71ead31e23a5cb6c9ace827f
MehulSolanki <er.mehulsolanki1887@gmail.com>
2024-11-19 12:45:58 +0000

    Create token_node01.sh

---
+++ token_node01.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+
+yes y | ssh-keygen -t rsa -N "" -f /home/myadmin/.ssh/node01 > /dev/null
+
+eval "$(ssh-agent -s)"
+
+ssh-add /home/myadmin/.ssh/node01
+
+apt install sshpass
+
+for ip in "172.20.1.11"; do sshpass -p Admin@123456 ssh-copy-id  -oStrictHostKeyChecking=no "myadmin@172.20.1.10" > /dev/null; done
+
+scp myadmin@172.20.1.10:/home/myadmin/token.sh /home/myadmin/
+
+sh /home/myadmin/token.sh
