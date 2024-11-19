3f0ddf49cdb150d598a67ec4b8c71297ad4bf46f
MehulSolanki <er.mehulsolanki1887@gmail.com>
2024-11-19 12:52:32 +0000

    Update k8s_cluster_workernode03.sh

--- k8s_cluster_workernode03.sh
+++ k8s_cluster_workernode03.sh
@@ -54,13 +54,3 @@ sudo apt-mark hold kubelet kubeadm kubectl
 sudo systemctl daemon-reload
 sudo systemctl start kubelet
 sudo systemctl enable kubelet.service
-
-sudo su
-
-yes y | ssh-keygen -t rsa -N "" -f /home/myadmin/.ssh/node03 > /dev/null
-eval "$(ssh-agent -s)"
-ssh-add /home/myadmin/.ssh/node03
-apt install sshpass
-for ip in "172.20.1.13"; do sshpass -p Admin@123456 ssh-copy-id  -oStrictHostKeyChecking=no "myadmin@172.20.1.10" > /dev/null; done
-scp myadmin@172.20.1.10:/home/myadmin/token.sh /home/myadmin/
-sh /home/myadmin/token.sh
