ae97ea6902b3f11692a708ad8edd795af3553702
MehulSolanki <er.mehulsolanki1887@gmail.com>
2024-11-28 10:32:53 +0000

    Update token_workernode02.sh

--- token_workernode02.sh
+++ token_workernode02.sh
@@ -8,7 +8,7 @@ ssh-add /home/myadmin/.ssh/node01
 
 apt install sshpass
 
-for ip in "172.20.1.12"; do sshpass -p Admin@123456 ssh-copy-id  -oStrictHostKeyChecking=no "myadmin@172.20.1.10" > /dev/null; done
+for ip in "172.30.1.12"; do sshpass -p Admin@123456 ssh-copy-id  -oStrictHostKeyChecking=no "myadmin@172.30.1.10" > /dev/null; done
 
 scp myadmin@172.20.1.10:/home/myadmin/token.sh /home/myadmin/
 
